1f5e3fe76a4e97407b067ac33df2a73f5282f062
Hervey Jair Quiñones Valentin <hquinones723@outlook.com>
2021-06-27 14:25:33 -0500

    clase 02

---
+++ Clase02/Clase02/EjercicioVenta/Venta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioVenta
+{
+    [Table("tbVenta")]
+    public class Venta
+    {
+        [Key]
+        public int IdVenta { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public int IdCliente { get; set; }
+
+        public Cliente Clientes { get; set; }
+
+        public List<DetalleVenta> DetalleVenta { get; set; }
+    }
+}
