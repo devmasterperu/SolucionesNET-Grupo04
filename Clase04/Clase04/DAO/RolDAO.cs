3d4bbba0f9e4910b49f952dafb4791ce500b230d
Hervey Jair Quiñones Valentin <hquinones723@outlook.com>
2021-07-15 22:05:18 -0500

    Clase 04-2

--- Clase04/Clase04/DAO/RolDAO.cs
+++ Clase04/Clase04/DAO/RolDAO.cs
@@ -24,7 +24,7 @@ namespace Clase04.DAO
             {
                 using (var conexion = new SqlConnection(_stringConnection))
                 {
-                    using (var comando = new SqlCommand("SP_ROL_List",conexion) )
+                    using (var comando = new SqlCommand("SP_ROL_List", conexion))
                     {
                         comando.CommandType = CommandType.StoredProcedure;
                         conexion.Open();
@@ -48,6 +48,91 @@ namespace Clase04.DAO
                 Console.WriteLine(ex.ToString());
             }
             return lista;
+        }
+        public Boolean RegistrarRol(RolBEAN rol)
+        {
+            bool rpta = false;
+            try
+            {
+                using (var conexion = new SqlConnection(_stringConnection))
+                {
+                    using (var comando = new SqlCommand("SP_ROL_Insert", conexion))
+                    {
+                        comando.CommandType = CommandType.StoredProcedure;
+                        comando.Parameters.AddWithValue("@nombreRol", rol.NombreRol);
+                        conexion.Open();
+                        comando.ExecuteNonQuery();
+                        rpta = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return rpta;
+        }
+        public List<RolBEAN> RegistroListadoRol(RolBEAN pRolBEAN)
+        {
+            /*CREATE PROCEDURE SP_ROL_InsertListar
+(@nombreRol varchar(20))
+as
+INSERT INTO tb_Rol(nombreRol)
+values(@nombreRol)
+
+select idRol, nombreRol from tb_Rol*/
+            List<RolBEAN> lista = new List<RolBEAN>();
+            RolBEAN rol;
+            try
+            {
+                using (var conexion = new SqlConnection(_stringConnection))
+                {
+                    using (var comando = new SqlCommand("SP_ROL_InsertListar", conexion))
+                    {
+                        comando.CommandType = CommandType.StoredProcedure;
+                        comando.Parameters.AddWithValue("@nombreRol", pRolBEAN.NombreRol);
+                        conexion.Open();
+                        using (var datos = comando.ExecuteReader())
+                        {
+                            while (datos.Read())
+                            {
+                                rol = new RolBEAN();
+                                rol.IdRol = Convert.ToInt32(datos[0]);
+                                rol.NombreRol = Convert.ToString(datos[1]);
+                                lista.Add(rol);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return lista;
+        }
+        public RolBEAN BuscarRolByID(int idRol)
+        {
+            RolBEAN rol = new RolBEAN();
+            using (var conexion = new SqlConnection(_stringConnection))
+            {
+                using (var comando = new SqlCommand("SP_ROL_ByID", conexion))
+                {
+                    comando.CommandType = CommandType.StoredProcedure;
+                    comando.Parameters.AddWithValue("@idRol", idRol);
+                    conexion.Open();
+                    using (var datos = comando.ExecuteReader())
+                    {
+                        if (datos.Read())
+                        {
+                            rol.IdRol = Convert.ToInt32(datos[0]);
+                            rol.NombreRol = datos[1].ToString();
+                        }
+                    }
+                }
+            }
+            return rol;
         }
     }
 }
--- Clase04/Clase04/Program.cs
+++ Clase04/Clase04/Program.cs
@@ -12,13 +12,95 @@ namespace Clase04
     {
         static void Main(string[] args)
         {
-            List<RolBEAN> ListaRol = new List<RolBEAN>();
-            RolDAO rolDAO = new RolDAO();
-            ListaRol = rolDAO.ListaRoles();
-            foreach (var item in ListaRol)
+            string rpta = "N";
+            do
             {
-                Console.WriteLine(item.IdRol + "\t" + item.NombreRol);
-            }
+                RolDAO rolDAO = new RolDAO();
+                Console.Clear();
+                Console.WriteLine("Mantenimiento de Roles");
+                Console.WriteLine("1 - Lista Roles ADO.NET");
+                Console.WriteLine("5 - Lista Roles EF");
+                Console.WriteLine("2 - Registro Roles");
+                Console.WriteLine("3 - Registro & Listado de Roles");
+                Console.WriteLine("4 - Buscar por ID");
+                Console.WriteLine("0 - Salir");
+                Console.Write("\nIngrese Opción: ");
+                int opcion;
+                opcion =Convert.ToInt32(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        List<RolBEAN> ListaRol = new List<RolBEAN>();
+                        ListaRol = rolDAO.ListaRoles();
+                        Console.Clear();
+                        Console.WriteLine("Lista Roles");
+                        foreach (var item in ListaRol)
+                        {
+                            Console.WriteLine(item.IdRol + "\t" + item.NombreRol);
+                        }
+                        Console.Write("¿Desea continuar? (S/N) ");
+                        rpta = Console.ReadLine();
+                        break;
+                    case 2:
+                        RolBEAN rolBEAN = new RolBEAN();
+                        Console.Write("Ingres nombre de Rol: ");
+                        rolBEAN.NombreRol = Console.ReadLine();
+                        bool rptaReg = rolDAO.RegistrarRol(rolBEAN);
+                        if (rptaReg)
+                        {
+                            Console.WriteLine("Registrado Correctamente");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error en registro de Rol");
+                        }
+                        Console.Write("¿Desea continuar? (S/N) ");
+                        rpta = Console.ReadLine();
+                        break;
+                    case 3:
+                        RolBEAN rolBEAN2 = new RolBEAN();
+                        Console.Write("Ingres nombre de Rol: ");
+                        rolBEAN2.NombreRol = Console.ReadLine();
+                        List<RolBEAN> listaRol = rolDAO.RegistroListadoRol(rolBEAN2);
+                        foreach (var item in listaRol)
+                        {
+                            Console.WriteLine(item.IdRol + "\t" + item.NombreRol);
+                        }
+                        Console.Write("¿Desea continuar? (S/N) ");
+                        rpta = Console.ReadLine();
+                        break;
+                    case 4:
+                        Console.Write("Ingrese IdRol a buscar: ");
+                        int id = Convert.ToInt32(Console.ReadLine());
+                        RolBEAN rolBEAN3 = new RolBEAN();
+                        rolBEAN3 = rolDAO.BuscarRolByID(id);
+                        if (rolBEAN3.IdRol == 0)
+                        {
+                            Console.WriteLine("Los registros con el Id en mención no existen");
+                        }
+                        else
+                        {
+                            Console.WriteLine(rolBEAN3.IdRol + " " + rolBEAN3.NombreRol);
+                        }
+
+                        Console.Write("¿Desea continuar? (S/N) ");
+                        rpta = Console.ReadLine();
+                        break;
+                    case 5:
+                        using (var db = new connBD_CONTACTABILIDAD())
+                        {
+                            List<tb_Rol> listaRoles = db.tb_Rol.ToList();
+                            foreach (var item in listaRoles)
+                            {
+                                Console.WriteLine(item.idRol + " " + item.nombreRol);
+                            }
+                        }
+                        Console.Write("¿Desea continuar? (S/N) ");
+                        rpta = Console.ReadLine();
+                        break;
+                }
+
+            } while (rpta == "S" || rpta == "s");
         }
     }
 }
